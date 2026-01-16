6347c9f5e9b6903048d09497fad9245b5e184a91
lemr <mlemrissani1056@alumnes.politecnicllevant.cat>
2026-01-16 22:42:14 +0100

    Creación de locations controller

---
+++ src/main/java/com/nautik/api/controller/locations/LocationsController.java
@@ -0,0 +1,14 @@
+package com.nautik.api.controller.locations;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/locations/")
+public class LocationsController {
+
+
+
+
+}
