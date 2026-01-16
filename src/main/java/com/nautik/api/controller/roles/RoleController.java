8fc1c6aa0c11e7eae9f242dc6e670eb67b9d49ba
lemr <mlemrissani1056@alumnes.politecnicllevant.cat>
2026-01-16 17:07:26 +0100

    Primera prueba de post para creació de rol

--- src/main/java/com/nautik/api/controller/roles/RoleController.java
+++ src/main/java/com/nautik/api/controller/roles/RoleController.java
@@ -1,7 +1,9 @@
 package com.nautik.api.controller.roles;
 
+import com.nautik.api.dto.roles.RoleCreateDto;
 import com.nautik.api.service.roles.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +14,17 @@ public class RoleController {
     @Autowired
     private RoleService roleService;
 
+
+    @PostMapping("/{companyId}/")
+    public String createRole(
+            @PathVariable int companyId,
+            @RequestBody RoleCreateDto roleCreateDto
+            ){
+        System.out.println(roleCreateDto.getCapabilities());
+
+
+        return "Ok";
+    }
 
 
 }
--- src/main/java/com/nautik/api/dto/roles/RoleCreateDto.java
+++ src/main/java/com/nautik/api/dto/roles/RoleCreateDto.java
@@ -1,6 +1,7 @@
 package com.nautik.api.dto.roles;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.List;
@@ -10,5 +11,7 @@ public class RoleCreateDto {
     String name;
     String description;
     //List for the capabilities id if each role, has to be passed as an array
+
+    @JsonProperty("capabilities")
     List<Integer> capabilities;
 }
