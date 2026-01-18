4aeef7c781b7b112741fd898aa5b9254db1c1118
Pedro <prubio82@alumnes.politecnicllevant.cat>
2026-01-18 15:34:50 +0100

    SCRUM 8 creacio de controllers, y amb el esquelet del service de port i zone

---
+++ src/main/java/com/nautik/api/controller/port/PortController.java
@@ -0,0 +1,48 @@
+package com.nautik.api.controller.port;
+
+import com.nautik.api.dto.port.PortDto;
+import com.nautik.api.service.port.PortService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ports")
+@RequiredArgsConstructor
+public class PortController {
+
+    private final PortService portService;
+
+    @GetMapping
+    public List<PortDto> getAllPorts() {
+        return portService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public PortDto getPortById(@PathVariable Long id) {
+        return portService.findById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public PortDto createPort(@RequestBody PortDto dto) {
+        return portService.create(dto);
+    }
+
+    @PutMapping("/{id}")
+    public PortDto updatePort(
+            @PathVariable Long id,
+            @RequestBody PortDto dto
+    ) {
+        return portService.update(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletePort(@PathVariable Long id) {
+        portService.delete(id);
+    }
+}
+
---
+++ src/main/java/com/nautik/api/service/location/ZoneService.java
@@ -0,0 +1,30 @@
+package com.nautik.api.service.location;
+
+import com.nautik.api.dto.location.ZoneDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ZoneService {
+
+    public List<ZoneDto> findByPort(Long portId){
+        return null;
+    }
+
+    public ZoneDto findById(Long portId, Long zoneId){
+        return null;
+    }
+    public ZoneDto create(Long portId, ZoneDto zone){
+        return null;
+    }
+
+    public ZoneDto update(Long portId, Long zoneId, ZoneDto zone ){
+        return null;
+    }
+
+    public void delete(Long portId, Long zondeId){
+    }
+
+
+}
