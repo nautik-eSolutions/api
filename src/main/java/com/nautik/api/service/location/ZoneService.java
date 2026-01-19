cc2a0a7e5d9074842531035348e13a19813046bc
PedroRubio14 <prubio82@alumnes.politecnicllevant.cat>
2026-01-19 22:35:49 +0100

    -

--- src/main/java/com/nautik/api/service/location/ZoneService.java
+++ src/main/java/com/nautik/api/service/location/ZoneService.java
@@ -1,12 +1,20 @@
 package com.nautik.api.service.location;
 
 import com.nautik.api.dto.location.ZoneDto;
+import com.nautik.api.repository.location.ZoneRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ZoneService {
+
+    private final ZoneRepository zoneRepository;
+    private final ModelMapper modelMapper;
 
     public List<ZoneDto> findByPort(Long portId){
         return null;
