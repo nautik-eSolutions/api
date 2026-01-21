3048a59e3d60d0e496932398c060f5c2a4e6379d
Pedro <prubio82@alumnes.politecnicllevant.cat>
2026-01-21 10:27:22 +0100

    port repository

--- src/main/java/com/nautik/api/dto/port/PortDto.java
+++ src/main/java/com/nautik/api/dto/port/PortDto.java
@@ -1,12 +1,19 @@
 package com.nautik.api.dto.port;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.Value;
 
 import java.io.Serializable;
 
 
-@Value
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PortDto implements Serializable {
     Integer id;
     String name;
+    String cityName;
+    String companyName;
 }
