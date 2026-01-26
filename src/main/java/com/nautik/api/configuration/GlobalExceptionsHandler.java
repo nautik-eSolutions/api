712db3be0c291a0c2578af4dee7daad9df9e08cd
mlemrissani <mlemrissani1056@alumnes.politecnicllevant.cat>
2026-01-26 10:09:34 +0100

    Creación de classe GlobalExceptionHandler

---
+++ src/main/java/com/nautik/api/configuration/GlobalExceptionsHandler.java
@@ -0,0 +1,7 @@
+package com.nautik.api.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GlobalExceptionsHandler {
+}
---
+++ src/main/java/com/nautik/api/domain/exceptions/UserNotFound.java
@@ -0,0 +1,9 @@
+package com.nautik.api.domain.exceptions;
+
+public class UserNotFound extends RuntimeException {
+
+
+    public UserNotFound(String message) {
+
+    }
+}
