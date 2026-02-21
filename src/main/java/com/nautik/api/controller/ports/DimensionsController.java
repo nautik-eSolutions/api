0b85099b2009a7c836791bc682da0901a2f66e38
mlemrissani <mlemrissani1056@alumnes.politecnicllevant.cat>
2026-02-21 16:23:08 +0100

    Implementación de dimensionsController

--- src/main/java/com/nautik/api/controller/ports/DimensionsController.java
+++ src/main/java/com/nautik/api/controller/ports/DimensionsController.java
@@ -1,12 +1,12 @@
 package com.nautik.api.controller.ports;
 
 import com.nautik.api.dto.mooring.MooringDimensionDto;
+import com.nautik.api.dto.mooring.create.MooringDimensionCreateDto;
+import com.nautik.api.service.port.MooringDimensionsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,12 +15,24 @@ import java.util.List;
 @RequiredArgsConstructor
 public class DimensionsController {
 
+    private final MooringDimensionsService mooringDimensionsService;
 
-    /*
     @GetMapping("/port/{portId}")
     public ResponseEntity<List<MooringDimensionDto>> getMooringDimensionsByPort(@PathVariable Integer portId){
-
+        return ResponseEntity.ok(mooringDimensionsService.getAllMooringsDimensionsByPort(portId));
     }
 
-     */
+    @PatchMapping("/{dimensionId}")
+    public ResponseEntity<MooringDimensionDto> updateMooringDimension(@PathVariable Integer dimensionId, @RequestBody MooringDimensionCreateDto dimensionCreateDto){
+        return ResponseEntity.ok(mooringDimensionsService.updateMooringDimension(dimensionId,dimensionCreateDto));
+    }
+    @PostMapping("/port/{portId}")
+    public ResponseEntity<MooringDimensionDto> createMooringDimension(@PathVariable Integer portId, @RequestBody MooringDimensionCreateDto dimensionCreateDto){
+        return ResponseEntity.ok(mooringDimensionsService.createMooringDimension(portId, dimensionCreateDto));
+    }
+    @DeleteMapping("/{dimensionId}")
+    public ResponseEntity<Void> createMooringDimension(@PathVariable Integer dimensionId ){
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(mooringDimensionsService.createMooringDimension(dimensionId));
+    }
+}
 }
