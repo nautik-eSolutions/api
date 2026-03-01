fc852e07c9260183bc9239d4ab16ceb1a62dca54
mlemrissani <mlemrissani1056@alumnes.politecnicllevant.cat>
2026-03-01 10:30:58 +0100

    Query para sacar todos los check-in de un dia y checkout

--- src/main/java/com/nautik/api/repository/bookings/CheckInOutRepository.java
+++ src/main/java/com/nautik/api/repository/bookings/CheckInOutRepository.java
@@ -12,14 +12,21 @@ import java.util.Optional;
 public interface CheckInOutRepository extends JpaRepository<CheckInOut,Integer> {
     Optional<CheckInOut> findByBookingId(Integer bookingId);
 
+    @Query("select cio from CheckInOut cio " +
+            "inner join Booking b on cio.booking =b " +
+            "inner join Mooring m on b.mooring = m " +
+            "inner join MooringCategory mc on m.mooringCategory = mc " +
+            "inner join Zone z on mc.zone = z " +
+            "inner join Port p on z.port = p " +
+            "where extract(DATE from b.startDate) = extract(DATE from ?1)  and p.id = ?2")
     List<CheckInOut> findCheckInsByDateAndPort(Date date,  Integer portId);
+
+    @Query("select cio from CheckInOut cio " +
+            "inner join Booking b on cio.booking =b " +
+            "inner join Mooring m on b.mooring = m " +
+            "inner join MooringCategory mc on m.mooringCategory = mc " +
+            "inner join Zone z on mc.zone = z " +
+            "inner join Port p on z.port = p " +
+            "where extract(DATE from b.endDate) = extract(DATE from ?1)  and p.id = ?2")
     List<CheckInOut> findCheckOutsByDateAndPort( Date date,  Integer portId);
-
-    List<CheckInOut> findByDateRangeAndPort(
-            Date startDate,
-           Date endDate,
-             Integer portId
-    );
-
-    Optional<CheckInOut> findByIdAndPortId( Integer occupancyId,  Integer portId);
 }
