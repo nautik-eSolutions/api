fdb66fe4a5fb414f545ca3b48ab23b7b6fef50b1
mlemrissani <mlemrissani1056@alumnes.politecnicllevant.cat>
2026-03-01 17:21:54 +0100

    Implementación de check in check out conectado con quasar

--- src/main/java/com/nautik/api/repository/bookings/CheckInOutRepository.java
+++ src/main/java/com/nautik/api/repository/bookings/CheckInOutRepository.java
@@ -18,7 +18,7 @@ public interface CheckInOutRepository extends JpaRepository<CheckInOut,Integer> 
             "inner join MooringCategory mc on m.mooringCategory = mc " +
             "inner join Zone z on mc.zone = z " +
             "inner join Port p on z.port = p " +
-            "where extract(DATE from b.startDate) = extract(DATE from ?1)  and p.id = ?2")
+            "where cast(b.startDate as date) = cast(  ?1 as date)  and p.id = ?2")
     List<CheckInOut> findCheckInsByDateAndPort(Date date,  Integer portId);
 
     @Query("select cio from CheckInOut cio " +
